834a0f4d25d7da42a0e8ef66e4a145a332656071
sabil <sabil.danish1997@gmail.com>
2024-08-13 19:51:52 +0530

    added a new message and another email instance

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/",async(req,res) => {
 })
 
 app.post("/sendEmail", async (req, res) => {
-  const { email } = req.body;
+  const { email, name } = req.body;
 
   let transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -26,9 +26,32 @@ app.post("/sendEmail", async (req, res) => {
     },
   });
 
-  const mailOptions = {
+  const mailOptionsOne = {
     from: "sabil.danish1997@gmail.com",
     to: email,
+    subject: "Thank You for Booking Your Consultation!",
+    html:  `
+    <p>Dear ${name},</p>
+    <h1>Thank you for booking a consultation with us!</h1>
+    <p>We’re excited to assist you on your publication journey.</p>
+    <p>Our team of experts is dedicated to helping you achieve your research goals. Whether you’re looking for guidance on manuscript preparation, journal selection, or submission assistance, we’re here to provide the support you need.</p>
+    <h2>What Happens Next:</h2>
+    <ul>
+      <li><strong>Consultation:</strong> One of our publication consultants will reach out to you within [24-48 hours] to schedule your consultation.</li>
+      <li><strong>Personalized Support:</strong> During the consultation, we’ll discuss your specific needs and outline how we can best support your publication goals.</li>
+    </ul>
+    <p>In the meantime, feel free to explore our website <a href="https://www.manuscriptedit.com">www.manuscriptedit.com</a> for tips and insights on successful publishing.</p>
+    <p>If you have any immediate questions, don’t hesitate to contact us at <a href="mailto:success@manuscriptedit.com">success@manuscriptedit.com</a>.</p>
+    <p>We look forward to working with you!</p>
+    <p>Best regards,</p>
+    <p>Lauren</p>
+    <p>Manuscriptedit Editorial (Operation)</p>
+  `,
+  };
+
+  const mailOptionsTwo = {
+    from: "sabil.danish1997@gmail.com",
+    to: "support@manuscriptedit.com",
     subject: "Email Received",
     html:
       "<h1>Your Email is Received</h1><p>We have received your email: " +
@@ -37,7 +60,9 @@ app.post("/sendEmail", async (req, res) => {
   };
 
   try {
-    let info = await transporter.sendMail(mailOptions);
+    let info_client = await transporter.sendMail(mailOptionsOne);
+    let info_admin = await transporter.sendMail(mailOptionsTwo);
+
     res.json({ message: "success" });
   } catch (error) {
     console.error("Error sending email: ", error);
